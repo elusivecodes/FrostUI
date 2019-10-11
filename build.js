6a5fb36c5a494162cff56305e5aa9c0e4e390e36
elusivecodes <18050480+elusivecodes@users.noreply.github.com>
2019-10-11 20:34:05 +1000

    Replaced uglify with terser and fixed some minor issues

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const filepath = require('filepath');
-const uglify = require('uglify-es');
+const terser = require('terser');
 const babel = require('@babel/core');
 const less = require('less');
 const cssmin = require('cssmin');
@@ -48,7 +48,12 @@ const code = wrapper.replace(
 );
 
 // minify
-const minified = uglify.minify(code);
+const minified = terser.minify(code, {
+    ecma: 8,
+    compress: {
+        ecma: 8
+    }
+});
 
 // write files
 if (minified.error) {
@@ -68,7 +73,9 @@ if (minified.error) {
 // es5 transpile
 const es5 = babel.transformSync(code, { presets: ['@babel/env'] });
 
-const minifiedes5 = uglify.minify(es5.code);
+const minifiedes5 = terser.minify(es5.code, {
+    ecma: 5
+});
 
 if (minifiedes5.error) {
     console.error(minifiedes5.error);
